650d62c74ef45529a45e631f4cff0cbb4b05dfc8
newhigen <newhigen@gmail.com>
2026-02-21 21:33:29 +0900

    deploy astro as github pages

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://newhigen.github.io/", // replace this with your deployed domain
   author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  profile: "https://newhigen.github.io/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
   ogImage: "astropaper-og.jpg",
